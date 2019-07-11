01c7a2209ba1bd8d8d49bc76cfcc65608d73e1a9
Megan Scarlett <scarlettutopia@aol.com>
2019-07-11 22:07:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,3 @@
 describe "#using_push" do
   it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do
-  countries_in_western_europe = ["Austria","Belgium","France","Germany"]  
+  countries_in_western_europe = ["Austria","Belgium","France","Germany"]
