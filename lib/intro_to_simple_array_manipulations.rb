d65ceb2bf6885f4a0bd14833ea66e2fc9a29b047
Megan Scarlett <scarlettutopia@aol.com>
2019-07-11 22:02:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,2 +1,3 @@
 describe "#using_push" do
-  it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method"
+  it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do
+    
